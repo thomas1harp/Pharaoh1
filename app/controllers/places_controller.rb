c306ed3e72bee14db3f495d3afb43dd657609568
Thomas Harp <thomas1.harp@yahoo.com>
2019-12-07 20:43:38 +0000

    Specified User Additions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
 
 	def index
 		@places = Place.paginate(:page => params[:page], :per_page => 10)
@@ -10,8 +10,8 @@ class PlacesController < ApplicationController
   	end
 
   	def create
-  		@place = Place.create(place_params)
-  		 redirect_to root_path
+  		current_user.places.create(place_params)
+  		redirect_to root_path
   	end
 
   	private
